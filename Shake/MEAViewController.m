369fadc78e459e029b8b27a25aacb90bf729890d
Mark Adams <mark.adams86@gmail.com>
2014-05-14 12:26:45 -0700

    Add shake functionality

--- Shake/MEAViewController.m
+++ Shake/MEAViewController.m
@@ -10,6 +10,9 @@
 
 @interface MEAViewController ()
 
+@property (nonatomic) UIDynamicAnimator *animator;
+@property (weak, nonatomic) IBOutlet UIView *box;
+
 @end
 
 @implementation MEAViewController
@@ -17,13 +20,26 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.animator = [UIDynamicAnimator new];
+
+    UISnapBehavior *snap = [[UISnapBehavior alloc] initWithItem:self.box snapToPoint:self.view.center];
+    snap.damping = 0.075;
+    [self.animator addBehavior:snap];
+
+    UIDynamicItemBehavior *behavior = [[UIDynamicItemBehavior alloc] initWithItems:@[self.box]];
+    behavior.allowsRotation = NO;
+    //behavior.resistance = 2;
+    behavior.density = 15;
+    [self.animator addBehavior:behavior];
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)shake:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UIPushBehavior *push = [[UIPushBehavior alloc] initWithItems:@[self.box] mode:UIPushBehaviorModeInstantaneous];
+    push.pushDirection = CGVectorMake(1, 0);
+    push.magnitude = 200;
+    [self.animator addBehavior:push];
+    push.active = YES;
 }
 
 @end
