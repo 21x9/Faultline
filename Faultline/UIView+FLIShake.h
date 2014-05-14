a8e9134a3699ff80f13abe69c5169766fd249053
Mark Adams <mark.adams86@gmail.com>
2014-05-14 12:39:03 -0700

    Rename to Faultline

---
+++ Faultline/UIView+FLIShake.h
@@ -0,0 +1,13 @@
+//
+//  UIView+FLIShake.h
+//  Shake
+//
+//  Created by Mark Adams on 5/14/14.
+//  Copyright (c) 2014 Mark Adams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (FLIShake)
+
+@end
---
+++ Faultline/UIView+FLIShake.m
@@ -0,0 +1,13 @@
+//
+//  UIView+FLIShake.m
+//  Shake
+//
+//  Created by Mark Adams on 5/14/14.
+//  Copyright (c) 2014 Mark Adams. All rights reserved.
+//
+
+#import "UIView+FLIShake.h"
+
+@implementation UIView (FLIShake)
+
+@end
